d6c1e85d2ada4d96576751d5f84335ab9954d9f4
Ranjan Kumar Singha <ranjansingha.coding@gmail.com>
2025-12-12 00:58:13 +0530

    DocuMind init

---
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,27 @@
+const SideBar = () => {
+  <>
+    <div className="demo-logo-vertical" />
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={["1"]}
+      items={[
+        {
+          key: "1",
+          icon: <UserOutlined />,
+          label: "nav 1",
+        },
+        {
+          key: "2",
+          icon: <VideoCameraOutlined />,
+          label: "nav 2",
+        },
+        {
+          key: "3",
+          icon: <UploadOutlined />,
+          label: "nav 3",
+        },
+      ]}
+    />
+  </>;
+};
